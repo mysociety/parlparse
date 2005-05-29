35d823cc1104b5e8f4e94455e3b3e1515e17ac9a
frabcus <frabcus@9cc54934-f9f6-0310-8a8d-e8e977684441>
2005-05-29 23:38:11 +0000

    Added some WTT constituencies. Changed Dunbartonshire default.
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@37 9cc54934-f9f6-0310-8a8d-e8e977684441

---
+++ members/wttcheck.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python2.3
+# vim:sw=4:ts=4:et:nowrap
+
+import datetime
+import sys
+import os
+
+sys.path.append("../pyscraper")
+from resolvemembernames import memberList
+
+today = datetime.date.today().isoformat()
+
+lines = file("../rawdata/wtt-constituencies.txt").readlines()
+
+for line in lines:
+    cons = line.strip()
+    cancons = memberList.canonicalcons(cons, today)
+    print cancons
+
+
+
