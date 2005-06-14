a2437493b485e8acc27a200fdfb60da1c5c6dd00
ukparsebot <ukparsebot@9cc54934-f9f6-0310-8a8d-e8e977684441>
2005-06-14 18:21:05 +0000

    More precise regex to catch all oral question numbers, grr.
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@66 9cc54934-f9f6-0310-8a8d-e8e977684441

--- pyscraper/debate/filterdebatespeakers.py
+++ pyscraper/debate/filterdebatespeakers.py
@@ -64,10 +64,10 @@ fixsubs = [
 
 parties = "|".join(map(string.lower, memberList.partylist())) + "|uup|ld|dup"
 # Rough match:
-recomb = re.compile('((?:Q?\d+\.\s*)?(?:\[\d+\]\s*)?(?:<stamp aname=".*?"/>)?<b>[^<]*</b>(?:\s*\((?:%s)\))?\s*:?)(?i)' % parties)
+recomb = re.compile('((?:Q?\d+\.\s*)?(?:\[\d+\]\s*)?(?:<stamp aname="[^"]*?"/>)?<b>[^<]*</b>(?:\s*\((?:%s)\))?\s*:?)(?i)' % parties)
 # Specific match:
 # Notes - sometimes party appears inside bold tags, so we match and throw it away on either side
-respeakervals = re.compile('(?:Q?(\d+)\.\s*)?(\[\d+\]\s*)?(<stamp aname=".*?"/>)?<b>\s*(?:Q?(\d+)\.)?([^:<(]*?):?\s*(?:\((.*?)\))?(?:\s*\((%s)\))?\s*:?\s*</b>(?:\s*\((%s)\))?(?i)' % (parties, parties))
+respeakervals = re.compile('(?:Q?(\d+)\.\s*)?(\[\d+\]\s*)?(<stamp aname="[^"]*?"/>)?<b>\s*(?:Q?(\d+)\.)?([^:<(]*?):?\s*(?:\((.*?)\))?(?:\s*\((%s)\))?\s*:?\s*</b>(?:\s*\((%s)\))?(?i)' % (parties, parties))
 
 # <B>Division No. 322</B>
 redivno = re.compile('<b>division no\. \d+</b>$(?i)')
