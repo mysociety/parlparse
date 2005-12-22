c414df2b58770ba53816d0ebfa3cfcc6c257a322
ukparsebot <ukparsebot@9cc54934-f9f6-0310-8a8d-e8e977684441>
2005-12-22 08:06:22 +0000

    Daily work autocommit
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@475 9cc54934-f9f6-0310-8a8d-e8e977684441

--- custom/edm2xml.pl
+++ custom/edm2xml.pl
@@ -3,26 +3,28 @@
 # parser which takes edmi.parliament.uk Early Day Motions and
 # converts them to an XML file.
 #
-# Still to do: 
-# 	include TWFY person identifiers
-
 use warnings;
 use strict;
-use LWP::UserAgent;
-use XML::Simple;
-my $browser = LWP::UserAgent->new;
-$browser->agent("www.TheyWorkForYou.com EDM fetcher - run by theyworkforyou\@msmith.net");
-my %EDM;
+
+
 my $base_url= 'http://edmi.parliament.uk/EDMi/';
 my $index_url= $base_url . 'EDMList.aspx';
+
+my $mplist_url= $base_url . 'MemberList.aspx?__EVENTTARGET=_alpha1:_';
 my $Parl_Session= '875'; # hardcode to 05-06 for now
 my $Parl_Session_readable= '05-06'; # hardcode to 05-06 for now
+use XML::Simple;
+use LWP;
+my $browser = LWP::UserAgent->new;
 my $dir=shift || die "usage: $0 <output dir>\n";
 my %MPmap;
-{
-	&parse_mpid_table();
-	&setup_cookies($index_url); # it'll get redirected to the front page anyway
-	&indexes_fetch($index_url, {});
+my %EDM;
+
+
+{ 
+	&setup_browser();
+	&mplist_fetch($mplist_url);
+	&indexes_fetch($index_url);
 
 	open (OUT, ">$dir/$Parl_Session_readable.xml") || die "can't open $dir/$Parl_Session_readable.xml:$!";
 	my $edm;
@@ -33,19 +35,15 @@ my %MPmap;
 	close (OUT);
 }
 
-sub setup_cookies {
-	my $url= shift;
-	$browser->cookie_jar({});
-	my $response = $browser->get($url);
-
-	# we don't need to do anything else
+sub setup_browser {
+    $browser->agent("www.TheyWorkForYou.com EDM fetcher - run by theyworkforyou\@msmith.net");
+    $browser->cookie_jar({});
 }
-
 
 
 sub indexes_fetch {
 	my $url= shift;
-	my $args= shift;
+	my $args= {};
 	$args->{'_MenuCtrl:ddlSession'} = $Parl_Session;
 	$args->{'ddlSortedBy'} = 1;
 	$args->{'ddlStatus'} = 0;
@@ -59,6 +57,30 @@ sub indexes_fetch {
 		die "Hmm, couldn't access it: ", $response->status_line, "\n";
 	}
 	#foreach my $k (sort keys %args){ print "\n\n\n$k - $args{$k}\n"; }
+}
+
+
+
+
+# fetch each index page of MPs and parse them all
+sub mplist_fetch {
+	my $url= shift;
+	my $args= shift;
+	$args->{'_MenuCtrl:ddlSession'} = $Parl_Session;
+	$args->{'ddlSortedBy'} = 1;
+	$args->{'ddlStatus'} = 0;
+	if (defined $ENV{DEBUG}) {print STDERR "Fetching $url\n";}
+	my $return='';
+	my $response = $browser->post($url, $args); # args that don't change each time
+	if($response->code == 200) {
+		my $content= $response->{_content};
+		#if (defined $ENV{DEBUG}) {print STDERR "$content\n";}
+		foreach my $letter ('a' .. 'z') {
+			$return.= &mp_list_parse($letter);
+		}
+	} else {
+		die "Hmm, couldn't access it: ", $response->status_line, "\n";
+	}
 }
 
 sub index_parse {
@@ -122,7 +144,7 @@ sub parse_motion {
 		$info_ref->{sponsored_by}->{$order}->{name}= $name;
 		#$info_ref->{sponsored_by}->{$order}->{position}= $order;
 		$info_ref->{sponsored_by}->{$order}->{edm_memberid}= $memberid;
-		$info_ref->{sponsored_by}->{$order}->{pw_memberid}= $MPmap{$memberid} || 'missing';
+		$info_ref->{sponsored_by}->{$order}->{pw_memberid}= $MPmap{$memberid}->{"signed_$info_ref->{parliament_edmid}"};
 	}
 	(@matches)= $content=~ m#<a\s*href='EDMByMember\.aspx\?MID=(\d+)'>\s*<span id="Sigs__ctl(\d+)_lblMember">(.*?)</span>#mcig;
 	while (($memberid, $order, $name, @matches)= @matches) {
@@ -131,23 +153,90 @@ sub parse_motion {
 		#$info_ref->{supported_by}->{$order}->{position}= $name;
 		$info_ref->{supported_by}->{$order}->{edm_memberid}= $memberid;
 		#$info_ref->{supported}->[$order -1]="$name";
-		$info_ref->{supported_by}->{$order}->{pw_memberid}= $MPmap{$memberid} || 'missing';
-	}
-	#print "$info_ref->{motion}\n";
+		$info_ref->{supported_by}->{$order}->{pw_memberid}= $MPmap{$memberid}->{"signed_$info_ref->{parliament_edmid}"};
+    } 
+        #print "$info_ref->{motion}\n";
 
 }
 
 
+sub mp_list_parse {
+	my $letter= shift;	
+	my $page= $browser->get($mplist_url . $letter);
+    if (defined $ENV{DEBUG}) {print STDERR "\tfetching $mplist_url$letter\n";}
+    my $lines='';
+	#print $page->content;
+	my (@parts, $mpid, $name, $constituency);
+	@parts = $page->content =~ m#<td><a href='EDMByMember\.aspx\?MID=(\d+).*?>([^>]+)</a></td>\s*<td>([^<]+)</td>#scg;
+    my $args;
 
-sub parse_mpid_table  {
-    my $line;
-	open (LIST, "$dir/people.txt") || die "can't open $dir/people.txt: $!";
-	while ($line = <LIST>) {
-		chomp($line);
-		#         "Constituency\tPIMS_MP_ID\tPublicWhipID\tName\n";
-		my ($constituency, $pims_id, $PW_id, $name) = split /\t/, $line;
-		$MPmap{$pims_id}=$PW_id;
-	}
-	close (LIST);
+	while (($mpid,$name,$constituency, @parts)= @parts) {
+        my $pwid;
+        &get_mp_page($mpid);
+        $args->{constituency}= $constituency;
+        $MPmap{$mpid}->{'constituency'}=$constituency;
+        $MPmap{$mpid}->{'name'}=$name;
+    }
+}
 
+
+
+
+sub get_mp_page {
+    my $mpid = shift; 
+    if (defined $ENV{DEBUG}) {print STDERR "getting mp page for $mpid\n";}
+	my $page = $browser->get('http://edmi.parliament.uk/EDMi/EDMByMember.aspx?MID='. $mpid);
+    my ($edmid, $edmname, $date);
+    my ($name)= $page->content=~ m#Member:</i> <span class="MTitle">\s*([^>]+?)\s*</span>#mi;
+    my ($constituency)= $page->content=~ m#Constituency:</i>&nbsp;<span>([^>]+?)</span>#mi;
+
+    if (defined $ENV{DEBUG}) {print STDERR "$name - $constituency\n";}
+
+    my (@matches) = $page->content =~ m#<tr.*?EDMDetails\.aspx\?EDMID=(\d+).*?td align="left"><a[^>]+>(.*?)</a></td>.*?(\d{2}\.\d{2}\.\d{4})#msigc;
+    if (defined $ENV{DEBUG}) {print STDERR "    matches count: $#matches\n";}
+    while (($edmid, $edmname, $date, @matches)= @matches) {
+        #print "here\n";
+        if (defined $ENV{DEBUG}) {print STDERR "    testing date $date for $mpid - edm ($edmid) $edmname\n";}
+        if (not defined $MPmap{$mpid}->{"signed_$date"}) {
+            my $pwid= &get_pwid_on_date($name, $constituency, $date,$mpid,$edmid);
+            $MPmap{$mpid}->{"signed_$edmid"}=$pwid;
+            $MPmap{$mpid}->{"date_signed_$edmid"}=$date;
+        if (defined $ENV{DEBUG}) {print STDERR "$name - $constituency - $date - $pwid\n";}
+        }
+    }
+}
+
+
+
+
+sub get_pwid_on_date {
+        my ($name, $constituency, $date, $mpid)=@_;
+        my $args;
+        my $pwid=0;
+        my $date_send=$date;
+        $name=~ s#\(.*?\)##;
+        if ($name =~ m/^(.*),(.*)$/) {
+            $args->{name}="$2 $1";
+        } else {
+            $args->{name}=$name;
+        }
+        if ($date =~ m#(\d{2})\.(\d{2})\.(\d{4})#) {
+            $date_send="$3-$2-$1";
+        }
+        $args->{constituency}= $constituency;
+        $args->{command}='mp-full-cons-match';
+        $args->{date}=$date_send;
+        if (defined $ENV{DEBUG}) {print STDERR "\tquerying $name for $date_send\n";}
+        my $response= $browser->post('http://ukparse.kforge.net/parlparse/rest.cgi', $args);
+
+		my @lines= split /\n/, $response->{_content};
+		#print $response->{_content};
+        if ($lines[1] eq 'OK') {
+            $lines[2]=~ m#member/(\d+)$#;
+            $pwid=$1; 
+        } else {
+            warn "Name match failed for  $args->{name} $constituency for $date";
+        } 
+        $MPmap{$mpid}->{"signed_$date"}=$pwid;
+        return ($pwid);
 }
