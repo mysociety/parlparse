08445a98630a2fa7e4efd75fd3c00201461c4bb9
fjmd1 <fjmd1@9cc54934-f9f6-0310-8a8d-e8e977684441>
2006-01-05 15:10:52 +0000

    billinfo.py now creates a list of acts that have received royal assent
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@524 9cc54934-f9f6-0310-8a8d-e8e977684441

--- pyscraper/minutes/billinfo.py
+++ pyscraper/minutes/billinfo.py
@@ -19,9 +19,13 @@ for vf in votesfiles:
 		votetree=ElementTree(file=vf)
 		voteroot=votetree.getroot()
 		date=voteroot.get('date')
+		m=re.match('(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})',date)
+		if not m:
+			print "internal error in date format"
+			sys.exit()
 		acts=votetree.findall('//royal_assent/act')
 		if len(acts)>0:
-			assent=Element('assent',{'date': date})
+			assent=Element('assent',m.groupdict())
 			for j in range(len(acts)):
 				assent.insert(j,acts[j])
 			topelement.insert(i,assent)
