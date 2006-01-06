fc729954d721cac0ca44f6f9b238691732b0746a
fjmd1 <fjmd1@9cc54934-f9f6-0310-8a8d-e8e977684441>
2006-01-06 12:46:02 +0000

    all currently scraped bill status pages now parse.
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@531 9cc54934-f9f6-0310-8a8d-e8e977684441

--- pyscraper/votes/parsebills.py
+++ pyscraper/votes/parsebills.py
@@ -37,48 +37,71 @@ def linktype(desc):
 	if re.match('Petitions against the(?P<billname>[-a-z.,A-Z0-9()\s]*?)Bill',desc):
 		return 'petitions'
 
+	if re.match('Report Stage Proceedings',desc):
+		return 'report'
+
+	if re.match('Committee of the Whole House Proceedings',desc):
+		return 'house committee'
+
 	return 'unknown'
 
-def makelement(gdict):
-	rest=gdict.pop('rest')
-	elem=Element('bill',gdict)
 
-	pat=re.compile('\s*<br>(\s|&nbsp;)*<a\s*href="(?P<href>[^"]*?)"\s*title="(?P<title>[^"]*?)"\s*>\s*<b>\s*<i>(?P<desc>[^<]*?)</i>\s*</b>\s*</a>(?i)')
+
+pat=re.compile('\s*(<br>)?(\s|&nbsp;)*<a\s*href="(?P<href>[^"]*?)"\s*title="(?P<title>[^"]*?)"\s*>\s*<b>\s*<i>(?P<desc>[^<]*?)(</i>\s*</b>\s*</a>|<br>)(?i)')
+
+def makelement(gdict,context):
+	rest=gdict.pop('rest')
+	elem=Element('bill',gdict)		
 
 	pos=0
 	#print rest
 	mobj=pat.match(rest)
 	while mobj and len(rest)>0:
 		pos=pos+1
+		rest=rest[mobj.end():]
+
 		linkdict=mobj.groupdict()
 		desc=linkdict.pop('desc')
 		desc=re.sub('\s+',' ',desc)
 		desc.strip()
 		type=linktype(desc)
+
+		mobj2=re.match('\s*\(to previous (print|version) of bill\)\s*',rest)
+		if mobj2:
+			type=type+'-previous'
+			rest=rest[mobj2.end():]
+	
 		linkdict.update({'desc':desc, 'type':type})
 		link=Element('link',linkdict)
-
 		elem.insert(pos,link)
 
-		rest=rest[mobj.end():]
 		mobj=pat.match(rest)
 
 		#print elementtree.ElementTree.tostring(elem)
 
-	print "remainder=%s\n" % rest
+	rest=re.sub('</TD>\s*</TR>\s*<tr><td>(&nbsp;|\s)*(?i)','',rest)
+	rest=re.sub('(<br>)?</TD>\s*</TR>\s*<TR valign=top>\s*<TD valign=top><a name="m"><FONT size=\+1><B>M</b></FONT></a><BR>(?i)','',rest)
+	rest=re.sub('\s*<br>\s*$','',rest)
+	rest=rest.strip()
+	if len(rest)>0:
+		raise Exception, "Additional material %s at bill %s:\n%s" % (context, gdict['billname1'], rest)
 
 	return elem
 		
 billpattern=re.compile('''<tr[^>]*>\s*<td[^>]*>\s*<img src="/pa/img/(?P<billtype>sqrgrn.gif|diamdrd.gif)"[^>]*></TD>\s*<TD><FONT size=\+1><A HREF="(?P<link>[^"]*)"\s*TITLE="Link to (?P<billname1>[-a-z.,A-Z0-9()\s]*?) Bill(\s*\[HL\]\s*)?"><B>(?P<billname2>[-a-z.,A-Z0-9()\s]*?) Bill(\s*\[HL\])?\s*\((?P<billno>\d+)\)\s*</B></A></FONT>(?P<rest>[\s\S]*?)</td></tr>(?i)''')
 
+mobj=re.search('bills\d{4}_(?P<date>\d{4}-\d{2}-\d{2})',sourcefilename)
+if not mobj:
+	print "fail",sourcefilename
+	sys.exit()
 
-billtreeroot=Element('billstatus')
+billtreeroot=Element('billstatus', mobj.groupdict())
 
 i=0
 m=billpattern.search(source)
 while m:
 	i=i+1
-	print i
+	#print i
 	#print m.groups()
 	gdict=m.groupdict()
 	if gdict['billtype']=='sqrgrn.gif':
@@ -87,7 +110,7 @@ while m:
 		billtype='lords'
 	gdict.update([('billtype',billtype)])
 
-	elem=makelement(gdict)
+	elem=makelement(gdict,sourcefilename)
 	billtreeroot.insert(i,elem)
 
 	source=source[m.end():]
@@ -97,5 +120,3 @@ billtree=ElementTree(billtreeroot)
 sourcename=sourcename.replace('.html','.xml')
 
 billtree.write('bills/%s' % sourcename)
-
-print i
