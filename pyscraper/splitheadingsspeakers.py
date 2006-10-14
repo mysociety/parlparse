d4d37b4a26f65bbce9cc8c64d7123c5d77e5e856
ukparsebot <ukparsebot@9cc54934-f9f6-0310-8a8d-e8e977684441>
2006-10-14 06:57:21 +0000

    Daily work autocommit
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@1460 9cc54934-f9f6-0310-8a8d-e8e977684441

--- pyscraper/splitheadingsspeakers.py
+++ pyscraper/splitheadingsspeakers.py
@@ -101,7 +101,7 @@ class StampUrl:
 regsection1 = '<h\d><center>.*?\s*</center></h\d>'
 regsection2 = '<h\d align="?center"?(?: style="text-transform:uppercase")?>.*?</h\d>'
 regsection3 = '(?:<p class="tabletext">(?:<stamp[^>]*>)*)?<center>(?:<stamp[^>]*>)*<b>.*?</b></center>'  # this case of title is used when quoting inserted amendment text that has a title in it.  This is reaching the limit of this type of parsing
-regsection4 = '<(?:p|br)>\s*<center>.*?</center><(?:p|br)>'
+regsection4 = '<(?:p|br)>\s*<center(?: style="text-transform:uppercase")?>.*?</center><(?:p|br)>'
 regsection5 = '<h[34] align=left>.*?</h[34]>'
 regparsermessage = '<parsemess.*?>' #'<parsemess-speech redirect="+-1"/>'
 regspeaker = '<speaker [^>]*>.*?</speaker>'
@@ -114,7 +114,7 @@ respeakerval = re.compile('<speaker ([^>]*)>.*?</speaker>')
 resectiont1val = re.compile('<h\d><center>\s*(.*?)\s*</center></h\d>(?i)')
 resectiont2val = re.compile('<h\d align="?center"?( style="text-transform:uppercase")?>\s*(.*?)\s*</h\d>(?i)')
 resectiont3val = re.compile('(<p class="tabletext">(?:<stamp[^>]*>)*)?<center>(?:<stamp[^>]*>)*<b>(.*?)</b></center>(?i)')
-resectiont4val = re.compile('<(?:p|br)>\s*<center>(.*?)</center><(?:p|br)>(?i)')
+resectiont4val = re.compile('<(?:p|br)>\s*<center(?: style="text-transform:uppercase")?>(.*?)</center><(?:p|br)>(?i)')
 
 # appears in today debates, and is getting a little too general (being able to mix/match b,i tags)
 resectiont5val = re.compile('<h[34] align=left>(?:<[bi]>)?\s*(.*?)\s*(?:</[bi]>)?</h\d>(?i)')
