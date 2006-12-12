e1735f266880158c3ebc4089014fc56f507b93d0
ukparsebot <ukparsebot@9cc54934-f9f6-0310-8a8d-e8e977684441>
2006-12-12 10:53:01 +0000

    Various parsing things
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@1658 9cc54934-f9f6-0310-8a8d-e8e977684441

--- pyscraper/runfilters.py
+++ pyscraper/runfilters.py
@@ -149,6 +149,7 @@ def RunFilterFile(FILTERfunction, xprev, sdate, sdatever, dname, jfin, patchfile
                 text = text.replace("\xc3\xb3", "&oacute;")
                 text = text.replace("\xc3\xb6", "&ouml;")
                 text = text.replace("\xc31/4", "&uuml;")
+                text = text.replace("\xc5\xa1", "&#353;");
                 text = re.sub('(<h5 align="left">)((?:<a name="(.*?)">)*)', r"\2\1", text) # If you can't beat them, ...
                 text = re.sub("(<br><b>[^:<]*:\s*column\s*\d+(?:WH)?\s*</b>)(\s+)(?i)", r"\1<br>\2", text)
                 text = re.sub("(\s+)(<b>[^:<]*:\s*column\s*\d+(?:WH)?\s*</b><br>)(?i)", r"\1<br>\2", text)
