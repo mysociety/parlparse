cdba199264918b649beb3ce0f641392bb0006e5a
louise <louise@9cc54934-f9f6-0310-8a8d-e8e977684441>
2007-02-09 10:00:32 +0000

    Initial check in of utilities for handling Public Bill Committees
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@1876 9cc54934-f9f6-0310-8a8d-e8e977684441

---
+++ pyscraper/standing/standingutils.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python2.4
+# Common utilities for handling Parliamentary Bill Committees
+#
+
+import re
+
+def construct_shortname(committeedate, letter, sittingnumber, sittingpart, date):
+    """Generate a shortname from document attributes"""
+    return "standing%s_%s_%02d-%d_%s" % (committeedate, letter, sittingnumber, sittingpart, date)
+
+def shortname_atts(shortname):  
+    """Get a tuples of committee attributes from shortname string"""
+    m = re.match("standing(?P<committeedate>.*?)_(?P<letter>[A-Z]|2)_(?P<sittingnumber>\d\d)-(?P<sittingpart>\d)_(?P<date>.*?)(?P<version>[a-z])$", shortname)
+    if not m:
+        raise Exception, "Attributes cannot be extracted from shortname %s" % shortname
+    return m.groupdict()
