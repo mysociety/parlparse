520eb7a30432efd741ae0afde4c6e2e372ddfdff
mark <mark@9cc54934-f9f6-0310-8a8d-e8e977684441>
2007-12-14 17:31:22 +0000

    The previous version of this accidentally ignored multiple-page-per day official reports.
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@3112 9cc54934-f9f6-0310-8a8d-e8e977684441

--- pyscraper/sp/get-official-reports.py
+++ pyscraper/sp/get-official-reports.py
@@ -13,7 +13,7 @@ from BeautifulSoup import NavigableString
 agent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)"
 
 class MyURLopener(urllib.FancyURLopener):
-        version = agent
+    version = agent
 
 urllib._urlopener = MyURLopener()
 
@@ -75,9 +75,9 @@ for year in range(1999,currentyear+1):
     html = fp.read()
     fp.close()
 
-    soup = BeautifulSoup( html )    
+    soup = BeautifulSoup( html )
     link_tags = soup.findAll( 'a' )
-    
+
     for t in link_tags:
 
         if t.has_key('href') and re.match('^or-',t['href']):
@@ -97,34 +97,76 @@ for year in range(1999,currentyear+1):
             page = str(t['href'])
 
             contents_url = official_reports_prefix + page
-            detail_url = re.sub( '01\.htm', '02.htm', contents_url )
-            if detail_url == contents_url:
-                raise Exception, "Contents page URL '%s' not recognised" % contents_url
 
-            urls = [ contents_url, detail_url ]
+            output_filename = official_report_template %  ( str(d), 0 )
+            if not os.path.exists(output_filename):
+                ur = urllib.urlopen(contents_url)
+                fp = open(output_filename, 'w')
+                fp.write(ur.read())
+                fp.close()
+                ur.close()
 
-            fetched_this_time = False
-            for i in [ 0, 1 ]:
+            fp = open(output_filename)
+            contents_html = fp.read()
+            fp.close()
 
-                output_filename = official_report_template %  ( str(d), i )
+            contents_soup = BeautifulSoup( contents_html )
+
+            detail_urls = []
+
+            report_id = None
+
+            for possible_report_link in contents_soup.findAll( 'a' ):
+                link = possible_report_link.get('href')
+                if not link:
+                    continue
+                m = re.match( '^(s?o[rR](.*)(\d{2})\.htm)', link )
+                if m and m.group(3) != "01":
+                    if report_id:
+                        if report_id != m.group(2):
+                            raise Exception, "report ID "+report_id+" and "+m.group(2)+" didn't match."
+                    else:
+                        report_id = m.group(2)
+                    if m.group(1) not in detail_urls:
+                        detail_urls.append(m.group(1))
+
+            if not detail_urls:
+                continue
+                        
+            detail_urls.sort()
+
+            all_urls = [ contents_url ]
+
+            for i in range(0,len(detail_urls)):
+
+                fetched_this_time = False
+
+                output_filename = official_report_template % ( str(d), i + 1 )
+                url_to_fetch = re.sub( '[^/]+$', detail_urls[i], contents_url )
+                all_urls.append(url_to_fetch)
 
                 if not os.path.exists(output_filename):
-                    fetched_this_time = True                    
-                    ur = urllib.urlopen(urls[i])
+                    fetched_this_time = True
+                    ur = urllib.urlopen(url_to_fetch)
                     fp = open(output_filename, 'w')
                     fp.write(ur.read())
                     fp.close()
                     ur.close()
+                else:
+                    # Already there, skipping...
+                    pass
 
-            if fetched_this_time:
-                # Write out the URLs of the contents and detail pages:
-                urls_filename = official_report_urls_template % str(d)
-                fp = open(urls_filename,"w")
-                for u in urls:
-                    fp.write(u)
-                    fp.write("\n")
-                fp.close()
-                # Sleep for a random time up to 2 minutes ...
-                amount_to_sleep = int( 120 * random.random() )
-                # print "sleeping for " + str(amount_to_sleep) + " seconds"
-                time.sleep( amount_to_sleep )
+                if fetched_this_time:
+                    # Sleep for a random time up to 2 minutes ...
+                    # amount_to_sleep = int( 120 * random.random() )
+                    amount_to_sleep = int( 20 * random.random() )
+                    # print "Sleeping for " + str(amount_to_sleep) + " seconds"
+                    time.sleep( amount_to_sleep )
+                    
+            # Write out the URLs of the contents and detail pages:
+            urls_filename = official_report_urls_template % str(d)
+            fp = open(urls_filename,"w")
+            for u in all_urls:
+                fp.write(u)
+                fp.write("\n")
+            fp.close()
