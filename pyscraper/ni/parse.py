cc55f27e055ba5b6d6e2d9a932de703ecd86a498
ukparsebot <ukparsebot@9cc54934-f9f6-0310-8a8d-e8e977684441>
2007-12-15 13:34:46 +0000

    Datetime tweak for NI; finally fixed that Liasion problem.
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@3123 9cc54934-f9f6-0310-8a8d-e8e977684441

--- pyscraper/ni/parse.py
+++ pyscraper/ni/parse.py
@@ -353,7 +353,7 @@ class ParseDay:
 			elif cl == 'Q1QuoteIndented' or cl == 'Q1Quote':
 				self.text += '<p class="indent">%s</p>\n' % phtml
 			elif cl == 'TimePeriod':
-				match = re.search('(\d\d?)\.(\d\d) ?(am|pm|noon)', ptext)
+				match = re.search('(\d\d?)\.\s*(\d\d) ?(am|pm|noon)', ptext)
 				hour = match.group(1)
 				if hour<12 and match.group(3) == 'pm':
 					hour += 12
