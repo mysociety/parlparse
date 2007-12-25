73b15cc05d999131e723153487c84293e4249dbb
ukparsebot <ukparsebot@9cc54934-f9f6-0310-8a8d-e8e977684441>
2007-12-25 08:32:42 +0000

    Daily work autocommit
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@3167 9cc54934-f9f6-0310-8a8d-e8e977684441

--- pyscraper/createhansardindex.py
+++ pyscraper/createhansardindex.py
@@ -178,6 +178,8 @@ def CmIndexFromPage(urllinkpage):
                 # 21st July 2005 has a link, but there was none
                 if uind == 'http://www.publications.parliament.uk/pa/cm200506/cmhansrd/vo050721/hallindx/50721-x.htm':
                         continue
+                if uind == 'http://www.publications.parliament.uk/pa/cm200708/cmhansrd/cm071203/hallindx/71203-x.htm':
+                        continue
                 # 21st June 2005 WHall links to 22nd June
                 if sdate=='2005-06-21' and uind=='http://www.publications.parliament.uk/pa/cm200506/cmhansrd/vo050622/hallindx/50622-x.htm':
                         uind = 'http://www.publications.parliament.uk/pa/cm200506/cmhansrd/vo050621/hallindx/50621-x.htm'
