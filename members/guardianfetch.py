077ae682a0a3ee75e713cbca3e4acbbe85f24b99
louise <louise@9cc54934-f9f6-0310-8a8d-e8e977684441>
2010-02-02 11:47:27 +0000

    Update for new person page format
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@8369 9cc54934-f9f6-0310-8a8d-e8e977684441

--- members/guardianfetch.py
+++ members/guardianfetch.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
-
+import sys
+sys.path.append("../pyscraper")
 import urllib, re
 base = 'http://politics.guardian.co.uk'
-
+from BeautifulSoup import BeautifulSoup
 out = open('../rawdata/mpinfo/guardian-mpsurls2005.txt', 'w')
 for i in range(-272, -266):
 	url = '%s/person/browse/mps/az/0,,%d,00.html' % (base, i)
@@ -15,9 +16,14 @@ for i in range(-272, -266):
 		name = '%s %s' % (match[2], match[1])
 		fp = urllib.urlopen(url)
 		person = fp.read()
-		fp.close()
-		cons = re.search('Member of Parliament for <a href="(/hoc/constituency/[^"]*)">(.*?)</a>', person)
+		mpurl = fp.geturl()
+                print "Parsing %s" % (mpurl)
+                fp.close()
+                soup = BeautifulSoup(person)
+                cons_div = soup.find('div', attrs={'id':'constituency'})
+                cons_link = cons_div.a
+                # first link in the constituency div
+		cons = re.search('<a href="([^"]*)">(.*?)</a>', str(cons_link))
 		consurl, consname = cons.groups()
-		consurl = '%s%s' % (base, consurl)
-		out.write('%s\t%s\t%s\t%s\n' % (name, consname, url, consurl))
+		out.write('%s\t%s\t%s\t%s\n' % (name, consname, mpurl, consurl))
 
