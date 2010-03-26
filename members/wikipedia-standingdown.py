1b34c40b94f62cdce57e77c6d37067de1435e7d4
ukparsebot <ukparsebot@9cc54934-f9f6-0310-8a8d-e8e977684441>
2010-03-26 18:17:30 +0000

    Get standingdown working again.
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@8471 9cc54934-f9f6-0310-8a8d-e8e977684441

--- members/wikipedia-standingdown.py
+++ members/wikipedia-standingdown.py
@@ -11,33 +11,23 @@ import datetime
 import sys
 import urlparse
 import re
-#from xml.dom import minidom
 
 sys.path.append("../pyscraper")
 from resolvemembernames import memberList
 
 today = datetime.date.today().isoformat()
 
-#mp_links = minidom.parse('wikipedia-commons.xml').getElementsByTagName('personinfo')
-#id_to_wikipedia = dict( (m.attributes['id'].value, m.attributes['wikipedia_url'].value) for m in mp_links )
-#wikipedia_to_id = dict( (v,k) for k, v in id_to_wikipedia.iteritems() )
-#
-# Get region pages
-page = open('../rawdata/Next_United_Kingdom_general_election').read()
-
-m = re.search('MPs not seeking re-election</span></h2>(.*?)<h2><span class="editsection">(?s)', page)
-if not m:
-    raise Exception, "Could not find Wikipedia section"
-section = m.group(1)
+page = open('../rawdata/MPs_standing_down_in_2010').read()
 
 print '''<?xml version="1.0" encoding="ISO-8859-1"?>
 <publicwhip>'''
-m = re.findall('<li><a href="([^"]*)"[^>]*>([^<]*)</a>', section)
+m = re.findall('<li><a href="([^"]*)"[^>]*>([^<]*)</a>', page)
 for row in m:
     url, name = row
-    if name == 'Iris Robinson': continue
-    id, canonname, canoncons = memberList.matchfullnamecons(name.decode('utf-8'), None, today) 
+    name = name.decode('utf-8')
+    if name in ('Iris Robinson', 'Ashok Kumar', 'David Taylor'): continue
+    id, canonname, canoncons = memberList.matchfullnamecons(name, None, today) 
     pid = memberList.membertoperson(id)
-    print '  <personinfo id="%s" standing_down="1" />' % pid
+    print ('  <personinfo id="%s" name="%s" standing_down="1" />' % (pid, name)).encode('iso-8859-1')
 print '</publicwhip>'
 
