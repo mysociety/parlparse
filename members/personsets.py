26eb2ce8594a0dea19dc9f84832c80b0b2d4a2d4
ukparsebot <ukparsebot@9cc54934-f9f6-0310-8a8d-e8e977684441>
2011-03-15 10:12:41 +0000

    Add new lords Glasman and Blencathra.
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@9149 9cc54934-f9f6-0310-8a8d-e8e977684441

--- members/personsets.py
+++ members/personsets.py
@@ -114,6 +114,7 @@ lordsmpmatches = {
     "uk.org.publicwhip/lord/101100" : "Michael Lord [Central Suffolk and North Ipswich]",
     "uk.org.publicwhip/lord/101108" : "Dafydd Wigley [Caernarfon]",
     "uk.org.publicwhip/lord/101118" : "Oona King [Bethnal Green and Bow]",
+    "uk.org.publicwhip/lord/101130" : "David Maclean [Penrith and The Border]",
 }
 
 # Put people who change party AND were MPs multple times in table above e.g. David Trimble
