e9501bb4c3ed309e01cd753c63a467678f51e442
ukparsebot <ukparsebot@9cc54934-f9f6-0310-8a8d-e8e977684441>
2012-07-04 08:50:34 +0000

    Add special case to avoid thinking something is someone withdrawing an amendment.
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@9906 9cc54934-f9f6-0310-8a8d-e8e977684441

--- pyscraper/lords/lordsfiltersections.py
+++ pyscraper/lords/lordsfiltersections.py
@@ -225,6 +225,9 @@ def MatchPWmotionStuff(qb, ispeechstartp1):
 	if re.match('<p>\[(?:<i>)?The page and line refer(?:ences are)? to .{0,160}?</p>', qpara):
 		return "misc"
 
+	# Needed to avoid lords on 2012-07-03 thinking this is someone withdrawing Amendment 63.
+	if re.match('<p>Amendment 63 has been withdrawn, so I turn now to', qpara):
+		return None
 
 	if re.match('<p>.{0,10}?(?:Amendment.{0,50}?|by leave, )(?<!semi-)withdrawn', qpara):
 		raise ContextException("Marginal withdrawn (fragment looks like it might be a withdrawn amendment, \nbut earlier regexp didn't pick it up)", stamp=qb.sstampurl, fragment=qpara)
