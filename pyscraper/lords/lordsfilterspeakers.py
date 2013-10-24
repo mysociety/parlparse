49c55a01f93b31429ddc17ec30dfd2ecba631115
ukparsebot <ukparsebot@9cc54934-f9f6-0310-8a8d-e8e977684441>
2013-10-24 09:57:38 +0000

    More Indie lords talking.
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@10732 9cc54934-f9f6-0310-8a8d-e8e977684441

--- pyscraper/lords/lordsfilterspeakers.py
+++ pyscraper/lords/lordsfilterspeakers.py
@@ -21,7 +21,7 @@ renormal = '<b>(?:<stamp aname="[^"]*"/>|</b><b>|[^<])*</b>(?:\s*:)?'
 respeaker = re.compile('(%s|%s|%s)(?i)' % (rehead, retable, renormal))
 
 respeakerb = re.compile('<b>\s*((?:<stamp aname="[^"]*"/>|</b><b>|[^<]+)*),?\s*</b>(\s*:)?(?i)')
-lord_parties = 'CB|Con|Lab|LD|Non-Afl|UUP|PC|Lab[ ]Ind'
+lord_parties = 'CB|Con|Lab|LD|Non-Afl|UUP|PC|Lab[ ]Ind|Ind[ ]Lab|Ind[ ]LD'
 respeakervals = re.compile('''
     ([^:(]*?)\s*
     (?:\(((?!%s)[^:)]*)\)?)?
