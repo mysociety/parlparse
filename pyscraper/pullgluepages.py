502db23150c604d672be0e89145ea0a46c58c4de
ukparsebot <ukparsebot@9cc54934-f9f6-0310-8a8d-e8e977684441>
2014-01-31 06:18:46 +0000

    Daily work autocommit
    git-svn-id: http://project.knowledgeforge.net/ukparse/svn/trunk@10911 9cc54934-f9f6-0310-8a8d-e8e977684441

--- pyscraper/pullgluepages.py
+++ pyscraper/pullgluepages.py
@@ -409,6 +409,7 @@ def ProcessIndexUrl(url, dgf, forcescrape):
         # import pdb;pdb.set_trace()
         for iconti in res:
                 uo = urlparse.urljoin(url, iconti)
+		print uo
                 if (not urla) or (urla[-1] != uo):
                         urla.append(uo)
 
