1ac5796e8c32dd34ad7e106f08a0762d7c18357b
Duncan Parkes <duncan@mysociety.org>
2016-02-11 10:35:08 +0000

    Allow ' and Wales' on the votes by parts of the country.
    For motions which affect only England and Wales, there is now
    a separate count of the votes from MPs from English and Welsh
    constituencies.

--- pyscraper/debate/divisionsections.py
+++ pyscraper/debate/divisionsections.py
@@ -25,7 +25,7 @@ housedivtxt = "The (?:House|Committee) (?:(?:having )?divided|proceeded to a Div
 rehousediv = re.compile('<p[^>]*>(?:<i>)?\s*%s(?:</i>|:|-)+ Ayes,? (\d+), Noes (\d+)\.</p>$' % housedivtxt)
 rehousediv_a = re.compile('<p[^>]*>(?:<i>)?\s*%s(?:</i>|:|-)+</p>$' % housedivtxt)
 rehousediv_b = re.compile('<p[^>]*>Ayes,? (\d+), Noes (\d+)\.</p>$')
-rehousediv_england = re.compile('<p[^>]*>(?:<i>)?Votes cast by Members for constituencies in England:(?:\s|</i>)*</p>$')
+rehousediv_england = re.compile('<p[^>]*>(?:<i>)?Votes cast by Members for constituencies in England(?: and Wales)?:(?:\s|</i>)*</p>$')
 
 foutdivisionreports = open(os.path.join(miscfuncs.tmppath, "divreport.html"), "w")
 #foutdivisionreports = None
