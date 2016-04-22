5d29a7158c7447b70efd3d7947005e63553412a8
Matthew Somerville <matthew@mysociety.org>
2016-04-22 11:02:10 +0100

    [UK] Tidy up process_hansard a bit.

--- pyscraper/process_hansard.py
+++ pyscraper/process_hansard.py
@@ -4,8 +4,6 @@
 import argparse
 import os
 import datetime
-import time
-import glob
 import re
 from os.path import join
 from miscfuncs import toppath
@@ -18,15 +16,6 @@ parser.add_argument('--from', dest='date_from', default=datetime.date.today().is
 parser.add_argument('--to', dest='date_to', default=datetime.date.today().isoformat(), metavar='YYYY-MM-DD')
 ARGS = parser.parse_args()
 
-
-def find(pattern, path):
-    result = []
-    for root, dirs, files in os.walk(path):
-        for name in files:
-            if re.search(pattern, name):
-                result.append(join(root, name).replace(zip_dir_slash, ''))
-    return result
-
 index_filename = join(toppath, 'seen_hansard_xml.txt')
 zip_directory = join(toppath, 'cmpages', 'hansardzips')
 zip_dir_slash = "%s/" % zip_directory
@@ -36,63 +25,70 @@ dir_match = '\d+_((\d{4}-\d{2}-\d{2})_\d{2}:\d{2}:\d{2})$'
 dirs = []
 for d in os.listdir(zip_directory):
     m = re.match(dir_match, d)
-    if m:
-        date = m.group(2)
-        if ARGS.date_from <= date <= ARGS.date_to:
-            dirs.append(join(zip_directory, d))
+    fn = join(zip_directory, d)
+    if m and os.path.isdir(fn) and ARGS.date_from <= m.group(2) <= ARGS.date_to:
+        dirs.append(fn)
 
-
-time_match = '.*/%s' % dir_match
-timeformat = '%Y-%m-%d_%H:%M:%S'
 # process the directories in date order so we do any revisions in the correct
 # order
-dirs.sort(key=lambda x: time.strptime(re.match(time_match, x).groups(1)[0], timeformat)[0:6])
+dirs.sort(key=lambda x: re.match('.*/%s' % dir_match, x).group(1))
+
 
 # make sure we only look at a file once
-entries = []
-if os.path.exists(index_filename):
-    with open(index_filename) as f:
-        entries = [e.strip().replace(zip_dir_slash, '') for e in f.readlines()]
+class Entries(list):
+    def __init__(self):
+        entries = []
+        if os.path.exists(index_filename):
+            with open(index_filename) as f:
+                entries = [e.strip().replace(zip_dir_slash, '') for e in f.readlines()]
+        super(Entries, self).__init__(entries)
 
-# in case the file is present but empty
-if entries is None:
-    entries = []
+    def dump(self):
+        with open(index_filename, 'w') as f:
+            f.writelines("{0}\n".format(entry) for entry in self)
+
+entries = Entries()
 
 
-def handle_file(parser, entries, filename, debate_type):
+def find(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if re.search(pattern, name):
+                result.append(join(root, name).replace(zip_dir_slash, ''))
+    return result
+
+
+def handle_file(filename, debate_type):
     file_key = '{0}:{1}'.format(debate_type, filename)
     if file_key in entries:
         print "already seen {0}, not parsing again".format(filename)
-        return entries, False
+        return False
 
+    parser.reset()
     parser.handle_file(join(zip_directory, filename), debate_type)
     print "parsed {0} file to {1}".format(debate_type, parser.output_file)
     entries.append(file_key)
 
-    return entries, True
+    return True
 
+parser = ParseDay()
 try:
     for d in dirs:
-        if os.path.isdir(d):
-            p = ParseDay()
-            xml_files = find('([CL]HAN|PBC).*\.xml$', d)
-            for x in xml_files:
-                print "parsing {0}".format(x)
-                p.reset()
-                if 'CHAN' in x:
-                    for dt in ['debate', 'westminhall']:
-                        entries, parsed = handle_file(p, entries, x, dt)
-                        p.reset()
-                elif 'LHAN' in x:
-                    entries, parsed = handle_file(p, entries, x, 'lords')
-                elif 'PBC' in x:
-                    entries, parsed = handle_file(p, entries, x, 'standing')
+        xml_files = find('([CL]HAN|PBC).*\.xml$', d)
+        for x in xml_files:
+            print "parsing {0}".format(x)
+            if 'CHAN' in x:
+                handle_file(x, 'debate')
+                handle_file(x, 'westminhall')
+            elif 'LHAN' in x:
+                handle_file(x, 'lords')
+            elif 'PBC' in x:
+                handle_file(x, 'standing')
 
 # this is just to make sure we record progress
 except Exception:
-    with open(index_filename, 'w') as f:
-        f.writelines("{0}\n".format(entry) for entry in entries)
+    entries.dump()
     raise
 
-with open(index_filename, 'w') as f:
-    f.writelines("{0}\n".format(entry) for entry in entries)
+entries.dump()
