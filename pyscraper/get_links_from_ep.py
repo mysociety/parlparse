bdd50b0defb05ff2da45bdefa63492981a61261e
Struan Donald <struan@exo.org.uk>
2017-02-14 15:29:25 +0000

    Fetch twitter and FB details from EP
    Use the everypolitician python module to fetch EP details for MPs, MLAs
    and MSPs and generate XML with twitter and facebook details for import
    by TWFY.
    
    Also adds a requirements.txt file for installing the relevant modules.

---
+++ pyscraper/get_links_from_ep.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import operator
+from lxml import etree
+from everypolitician import EveryPolitician
+
+
+def output_file(country, legislature, filename):
+    data = EveryPolitician().country(country).legislature(legislature)
+    output_filename = "../members/{0}.xml".format(filename)
+    root = etree.Element('publicwhip')
+
+    sorted_people = sorted(
+        data.popolo().persons,
+        key=operator.attrgetter('name')
+    )
+    for person in sorted_people:
+        parlparse_id = person.identifier_value('parlparse')
+        if parlparse_id is not None:
+            props = {}
+            if person.twitter:
+                props['twitter_username'] = person.twitter
+            if person.facebook:
+                props['facebook_page'] = person.facebook
+
+            if props:
+                props['id'] = parlparse_id
+                info = etree.Element('personinfo', props)
+                root.append(info)
+
+    et = etree.ElementTree(root)
+    et.write(output_filename, pretty_print=True)
+
+
+output_file('UK', 'Commons', 'social-media-commons')
+output_file('Scotland', 'Parliament', 'social-media-sp')
+output_file('Northern-Ireland', 'Assembly', 'social-media-ni')
