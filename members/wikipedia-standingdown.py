8b14c82d6b5e961e218cf013a27b7b4bca37ad37
Matthew Somerville <matthew@mysociety.org>
2024-05-24 12:23:53 +0100

    [UK] 2024 standing down list.

--- members/wikipedia-standingdown.py
+++ members/wikipedia-standingdown.py
@@ -13,19 +13,18 @@ import re
 sys.path.append("../pyscraper")
 from resolvemembernames import memberList
 
-today = '2010-04-12'
+today = '2024-05-24'
 
-page = open('../rawdata/MPs_standing_down_in_2010').read()
+page = open('../rawdata/Members_of_the_2024_standing_down').read()
+page = re.sub('(?s)^.*?<caption>Members of Parliament not standing for re-election', '', page)
+page = re.sub('(?s)</table>.*', '', page)
 
 print('''<?xml version="1.0" encoding="ISO-8859-1"?>
 <publicwhip>''')
-m = re.findall('<li><a href="([^"]*)"[^>]*>([^<]*)</a>', page)
+m = re.findall(r'<tr>\s*<td>.*?<a href="([^"]*)"[^>]*>([^<]*)</a>', page)
 for row in m:
     url, name = row
-    name = name.decode('utf-8')
-    if name in ('Iris Robinson', 'Ashok Kumar', 'David Taylor'): continue
-    id, canonname, canoncons = memberList.matchfullnamecons(name, None, today) 
-    pid = memberList.membertoperson(id)
-    print(('  <personinfo id="%s" name="%s" standing_down="1" />' % (pid, name)).encode('iso-8859-1'))
+    pid, canonname, canoncons = memberList.matchfullnamecons(name, None, today) 
+    print(('  <personinfo id="%s" name="%s" standing_down_2024="1" />' % (pid, name)))
 print('</publicwhip>')
 
