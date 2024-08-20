1cad2b1fab2a0c1f11e07c8098f8da3d9a4cece9
Struan Donald <struan@exo.org.uk>
2024-08-20 16:22:58 +0100

    [SP] add wrans download

--- pyscraper/sp_2024/__main__.py
+++ pyscraper/sp_2024/__main__.py
@@ -6,12 +6,14 @@ Use `python -m pyscraper.sp_2024 --help` to see available commands
 
 from __future__ import annotations
 
-from .download import fetch_debates_for_dates
-from .parse import tidy_up_html
-from .convert import convert_xml_to_twfy
-import click
-from pathlib import Path
 import datetime
+from pathlib import Path
+
+import click
+
+from .convert import convert_xml_to_twfy
+from .download import fetch_debates_for_dates, fetch_wrans_for_dates
+from .parse import tidy_up_html
 
 file_dir = Path(__file__).parent
 parldata = Path(file_dir, "..", "..", "..", "parldata")
@@ -111,6 +113,59 @@ def debates(
             if verbose:
                 print(f"Converting {file} to TheyWorkForYou format")
             convert_xml_to_twfy(file, output_dir, verbose=verbose)
+
+
+@cli.command()
+@click.option(
+    "--start-date", help="isodate to start fetching wrans from", required=True
+)
+@click.option("--end-date", help="isodate to end fetching wrans at", required=True)
+@click.option(
+    "--download",
+    is_flag=True,
+    help="Download the wrans, pair with 'override' to redownload all files",
+)
+@click.option("--parse", is_flag=True, help="Parse the downloaded wrans")
+@click.option("--convert", is_flag=True, help="Convert the parsed wrans")
+@click.option("--verbose", is_flag=True, help="Print verbose output")
+@click.option("--override", is_flag=True, help="Override existing files")
+@click.option(
+    "--partial-file-name", help="Only parse/convert files that match this string"
+)
+def wrans(
+    start_date: str,
+    end_date: str,
+    download: bool = False,
+    parse: bool = False,
+    convert: bool = False,
+    verbose: bool = False,
+    override: bool = False,
+    partial_file_name: str | None = None,
+):
+    """
+    Download written answers from Scottish Parliament between a start and end date.
+    """
+
+    start = datetime.date.fromisoformat(start_date)
+    end = datetime.date.fromisoformat(end_date)
+
+    # if none of the flags are set, error that at least one flag must be set
+    if not any([download, parse, convert]):
+        click.echo("At least one of the flags must be set")
+        return
+
+    # iterate through downloaded files if we're downloading them
+    # otherwise go find the relevant files based on name
+    if download:
+        file_iterator = fetch_wrans_for_dates(
+            start.isoformat(),
+            end.isoformat(),
+            verbose=verbose,
+            cache_dir=download_dir,
+            override=override,
+        )
+        for file in file_iterator:
+            pass
 
 
 if __name__ == "__main__":
