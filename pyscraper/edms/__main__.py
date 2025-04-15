ad143b82856cd4321ff5ac96074e5e68ee8afc6a
Alex Parsons <alex@alexparsons.co.uk>
2025-04-15 21:12:10 +0000

    Add EDM downloader
    - Initially downloads from 2015
    - nightly update of last 3 months
    - rebuilds two parquet files that can be instantly accessed.

---
+++ pyscraper/edms/__main__.py
@@ -0,0 +1,33 @@
+from typer import Typer
+
+from .commons import fetch_and_update, initial_populate, to_parquet
+
+app = Typer()
+
+
+@app.command(name="initial")
+def app_initial_populate():
+    """
+    Fetch and save all proposals from the start date to today.
+    """
+    initial_populate()
+
+
+@app.command(name="update")
+def app_fetch_and_update(quiet: bool = False):
+    """
+    Fetch and update the proposals from the last 90 days.
+    """
+    fetch_and_update(quiet=quiet)
+
+
+@app.command(name="parquet")
+def app_to_parquet():
+    """
+    Convert the proposals to parquet format.
+    """
+    to_parquet()
+
+
+if __name__ == "__main__":
+    app()
